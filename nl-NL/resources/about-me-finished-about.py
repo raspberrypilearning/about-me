4ade21c43c4b872c7455b7fb03db640fb001f71e
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-03-15 21:21:29 +0000

    New translations about-me-finished-about.py (Dutch)

---
+++ nl-NL/resources/about-me-finished-about.py
@@ -0,0 +1,25 @@
+#! / bin/python3
+
+print('Hoi, ik kan programmeren in Python!')
+
+print('''
+Schapen zijn mijn favoriete dieren
+
+ o-###-
+   | | #
+
+Ik woon in Glasgow
+
+   _|_
+  | |
+  |# |____
+  | | |
+  | #| # |
+ _|___|_#__|_
+
+''')
+
+born = input('In welk jaar ben je geboren?')
+born = int(born)
+age = 2025 - born
+print('In 2025 ben je', age, 'jaar!')

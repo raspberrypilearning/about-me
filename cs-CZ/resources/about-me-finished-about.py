cd3156e28385d77750becd9182b0658c05f2f5fd
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-03-19 12:53:58 +0000

    New translations about-me-finished-about.py (Czech)

---
+++ cs-CZ/resources/about-me-finished-about.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+print ('Ahoj, kóduju v Pythonu!')
+
+print('''
+Moje oblíbená zvířata jsou ovce
+
+   o-###-
+       | |   #
+
+Žiji v Glasgowě
+
+      _|_
+    |   |
+    |#  |____
+    |   |    |
+    |  #|  # |
+  _|___|_#__|
+
+''')
+
+narozen = input ("V jakém roce jsi se narodil?")
+narozen = int(narozen)
+vek = 2025 - narozen
+print('V roce 2025 ti bude', vek, 'let!')

aad7381bea3f7e1ff9ed6c2c953ac2c4222989ed
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-10-02 14:17:44 +0100

    New translations about-me-finished-about.py (Chinese Traditional)

--- zh-TW/resources/about-me-finished-about.py
+++ zh-TW/resources/about-me-finished-about.py
@@ -1,14 +1,14 @@
-# bin/python3
+#!/bin/python3
 
-print（“HI，我可以用Python寫程式！”）
+print('Hi, I can code in Python!')
 
 print('''
-我最喜歡的動物是綿羊
+My favourite animals are sheep
 
  o-###-
    | |   #
 
-我住在格拉斯哥
+I live in Glasgow
 
    _|_
   |   |
@@ -19,7 +19,7 @@ print('''
 
 ''')
 
-born = input('你在那一年出生?')
+born = input('What year were you born?')
 born = int(born)
 age = 2025 - born
-print('在2025年你將會\'ll be', age, '歲!')
+print('In the year 2025 you\'ll be', age, 'years old!')
